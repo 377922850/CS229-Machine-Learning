5d60657beca603d5ed8e5211d1c33089c078b880
377922850 <377922850@qq.com>
2017-05-05 21:05:32 +0800

    lrcostfunction

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,14 +36,15 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+J = -1/m*(y'*log(sigmoid(X*theta)) + (1-y')*log(1-sigmoid(X*theta))) + lambda/(2*m)*sum(theta(2:end).^2);
 
+grad = 1/m*(X'*(sigmoid(X*theta)-y))
+temp = theta; 
+temp(1) = 0;   % because we don't add anything for j = 0  
+grad = grad + lambda/m*temp;
 
-
-
-
-
-
-
+% grad = 1/m*(X'*(sigmoid(X*theta)-y)) + lambda/m*theta;
+% grad(1) = grad(1) - lambda/m*theta(1);
 
 % =============================================================
 
